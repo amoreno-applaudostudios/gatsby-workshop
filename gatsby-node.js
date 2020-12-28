dd46844bff360b7dc7be8c653509152d707968dc
windgart <amoreno@applaudostudios.com>
2020-12-28 15:38:57 -0600

    ✨ add planets section

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,37 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const ky = require("ky-universal")
+const fs = require("fs")
 
-// You can delete this file if you're not using it
+const URL = "https://swapi.dev/api/planets/"
+const planetsRoute = "./src/site_data/planets.json"
+const planets = require(planetsRoute)
+
+const getData = async () => {
+  try {
+    const response = await ky.get(URL).json()
+    const data = JSON.stringify(response, null, 2)
+    fs.writeFileSync(planetsRoute, data)
+    console.log("=== data created, saved to:", planets)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+exports.createPages = async ({ actions: { createPage } }) => {
+  await getData()
+  createPage({
+    path: "/planets",
+    component: require.resolve("./src/components/templates/planets"),
+    context: {
+      planetsData: planets,
+    },
+  })
+  planets.results.forEach(planet => {
+    createPage({
+      path: `/planets/${planet.name}`,
+      component: require.resolve("./src/components/templates/planet"),
+      context: {
+        planetInfo: planet,
+      },
+    })
+  })
+}
